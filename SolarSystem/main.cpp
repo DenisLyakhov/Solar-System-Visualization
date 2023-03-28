857f9a311c8543e365092c45a6470a006d3d1d9d
hub <hubmiz20@gmail.com>
2023-03-28 13:51:52 +0200

    Dodatkowe pliki

--- SolarSystem/main.cpp
+++ SolarSystem/main.cpp
@@ -4,8 +4,22 @@
 #include <stdlib.h>
 #include <DataLoader.h>
 #include <DataParser.h>
+#include <Planet.h>
+#include<DefaultCelestialBodyData.h>
 
 using namespace std;
+
+
+const map<string, string> DefaultCelestialBodyData::MERCURY = { {"CENTER","199@10"} };
+const map<string, string> DefaultCelestialBodyData::VENUS = { {"CENTER","299@10"} };
+const map<string, string> DefaultCelestialBodyData::EARTH = { {"CENTER","399@10"} };
+const map<string, string> DefaultCelestialBodyData::MARS = { {"CENTER","499@399"} };
+const map<string, string> DefaultCelestialBodyData::JUPITER = { {"CENTER","599@10"} };
+const map<string, string> DefaultCelestialBodyData::SATURN = { {"CENTER","699@10"} };
+const map<string, string> DefaultCelestialBodyData::URANUS = { {"CENTER","799@10"} };
+const map<string, string> DefaultCelestialBodyData::NEPTUNE = { {"CENTER","899@10"} };
+const map<string, string> DefaultCelestialBodyData::SUN = { {"CENTER","10@10"} };
+
 
 double rotation = 0.0;
 
@@ -57,24 +71,11 @@ void reshape(int width, int height) {
 
 int main(int argc, char** argv) {
 
-	// obj_name=Mars&location=500@399&start_time=now&stop_time=+1d&step_size=1h
 
-	DataLoader loader = DataLoader();
-	loader.setFormat("format=text");
-	loader.setCommand("COMMAND='499'");
-	loader.setObjData("OBJ_DATA='YES'");
-	loader.setCenter("CENTER='500@399'");
-	loader.setStartTime("START_TIME='2006-01-01'");
-	loader.setStopTime("STOP_TIME='2006-01-20'");
-	loader.setStepSize("STEP_SIZE='1%20d'");
-	loader.setQuantities("QUANTITIES='1,9,20,23,24,29'");
-	loader.setMakeEphem("MAKE_EPHEM='YES'");
-	loader.setEphemType("EPHEM_TYPE='VECTOR'");
+	Planet body = Planet("mars");
+	list<VectorData> vectorList = body.getVectorData();
 
-	cout << loader.buildUrl() << endl;
-	string data = loader.getData().text;
-	cout << DataParser::parseData(data) << endl;
-	list<VectorData> vectorList = DataParser::parseDataVector(data);
+
 	for (auto v : vectorList) {
 		cout << v.toString() << endl;
 	}
