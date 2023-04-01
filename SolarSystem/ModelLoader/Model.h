7b4eff455f7cb2db648c6883773888e4d474d787
hub <hubmiz20@gmail.com>
2023-04-01 13:55:13 +0200

    PierwszeZmiany

--- SolarSystem/ModelLoader/Model.h
+++ SolarSystem/ModelLoader/Model.h
@@ -1,9 +1,21 @@
-//vcpkg install nlohmann - json
+//vcpkg install assimp
 #pragma once
 
+#include<iostream>
+#include <assimp/Importer.hpp>      // C++ importer interface
+#include <assimp/scene.h>           // Output data structure
+#include <assimp/postprocess.h> 
+#include <assimp/cimport.h>
 
 
+class Model
+{
+	private:
+	const aiScene* scene;
+	Assimp::Importer importer;
 
-class Model {
+	struct Mesh {
+		unsigned int VAO, VBO2, VBO3, EBO; //Buffer
+	};
 
 };
